0114d1eb2bfd6d16f696ae6419c5a00050eed4b5
Luis Filipe <luis_filipe42@outlook.com>
2018-10-06 12:55:31 -0300

    Fixed Routes to Router.

--- public/product/src/routes.js
+++ public/product/src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const routes = express.Routes();
+const routes = express.Router();
 
-routes.get('/',(req,res) => {
+routes.get('/',(req, res) => {
     return res.send('Ola');
 });
 
