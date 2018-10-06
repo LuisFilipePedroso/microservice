e256a286fe06ef3630bf2d19881bf56a9b1a39b8
Luis Filipe <luis_filipe42@outlook.com>
2018-10-06 14:25:03 -0300

    Created store methods for client and product service.

---
+++ public/client/src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = express.Router();
+
+const ClientController = require('./controllers/ClientController');
+
+routes.get('/clients', ClientController.index);
+routes.post('/clients', ClientController.store);
+
+module.exports = routes;
